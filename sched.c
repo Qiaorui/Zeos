9eb79b40227991a21589e3e29817ce7fde4ba0e7
qiaorui <qiaorui.xiang@gmail.com>
2015-10-12 01:40:50 +0200

    4.4.1

--- sched.c
+++ sched.c
@@ -15,16 +15,18 @@ union task_union protected_tasks[NR_TASKS+2]
 
 union task_union *task = &protected_tasks[1]; /* == union task_union task[NR_TASKS] */
 
-#if 0
+//#if 0
 struct task_struct *list_head_to_task_struct(struct list_head *l)
 {
   return list_entry( l, struct task_struct, list);
 }
-#endif
+//#endif
 
 extern struct list_head blocked;
 struct list_head freequeue;
 struct list_head readyqueue;
+struct task_struct *idle_task;
+
 
 /* get_DIR - Returns the Page Directory address for task 't' */
 page_table_entry * get_DIR (struct task_struct *t) 
@@ -62,6 +64,17 @@ void cpu_idle(void)
 
 void init_idle (void)
 {
+  struct task_struct* tmp = list_head_to_task_struct(list_first(&freequeue));
+  list_del(list_first(&freequeue));
+  tmp->PID = 0;
+  allocate_DIR(tmp);
+  //cpu_idle();
+  idle_task = tmp;
+  
+  union task_union* tmp_task = (union task_union*)tmp;
+  tmp_task->stack[1023] = (int)cpu_idle;
+  tmp_task->stack[1022] = 0;
+  tmp_task->task.kernel_esp = &tmp_task->stack[1022];
 
 }
 
